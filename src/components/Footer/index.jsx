3fe1f5cc834e4e75f6da65514282cec8a5091285
Vinicius morais <vmorais34@gmail.com>
2022-02-14 20:55:10 -0300

    create Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+function Footer() {
+    return (
+        <footer>
+            <p>© Software Powered by Xtrategie</p>
+            <span>2021 - 2022</span>
+        </footer>
+)};
+
+export default Footer;
